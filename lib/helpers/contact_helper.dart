948fa01fb5cede58f3f66766ba8a2e97d24ebb25
Fillipe626 <lpjpa91@gmail.com>
2021-04-30 18:17:46 -0400

    Finalizado o banco e getters

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -40,6 +40,58 @@ class ContactHelper{ //usando o padrão de projeto singleton para evitar que o o
 
     });
   }
+  
+  Future<Contact> saveContact(Contact contact) async{
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+  
+  Future<Contact> getContact (int id) async{
+    Database dbContact = await db;
+    List<Map> maps= await dbContact.query(contactTable,
+    columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+     where:"$idColumn = ?",
+    whereArgs: [id]);
+
+    if(maps.length > 0){
+      return Contact.fromMap(maps.first);
+
+    }else{
+      return null;
+    }
+  }
+
+  Future<int>deleteContact (int id)async {
+    Database dbContact = await db;
+   await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+
+  }
+
+  Future<int>updateContact (Contact contact) async{
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?", whereArgs: [contact.id]);
+  }
+
+  Future<List>getAllContacts() async{
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");//lista de mapas
+    List<Contact> listContact = List();//declarada uma lista de contatos
+    for (Map m in listMap ){//pra cada mapa da minha lista de mapas
+      listContact.add(Contact.fromMap(m));//transformo em um contato
+    }
+    return listContact;
+  }
+
+  Future<int>getNumber() async{
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+
+ Future close () async{
+    Database dbContact = await db;
+    dbContact.close();
+  }
 
 }
 
