adc47e62b0ae2f21d77c0531d8478550decbdb62
Fillipe626 <lpjpa91@gmail.com>
2021-04-30 17:47:22 -0400

    Altecções na classe do banco

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,5 +1,8 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:async/async.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "phoneColumn";
@@ -7,7 +10,36 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 
-class ContactHelper{
+class ContactHelper{ //usando o padrão de projeto singleton para evitar que o objeto seja instanciado mais de uma vez
+  static final ContactHelper  _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db; //Apenas a classe contact helper vai poder mexer na classe
+
+  Future<Database>get db async{
+    if (_db != null){
+      return _db;
+
+    }else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, "contacts.db");
+
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{//será realizado apenas na primeira vez para iniciar o banco
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+      );
+
+    });
+  }
 
 }
 
@@ -42,4 +74,8 @@ Map toMap(){
     return map;
  }
 
+  @override //para dar print no contato
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+  }
 }
