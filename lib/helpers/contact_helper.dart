5854b05678423c32b6aa60a3cbd7aa87b0a9362b
Fillipe626 <lpjpa91@gmail.com>
2021-05-03 11:30:44 -0400

    Projeto finalizado

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -5,90 +5,89 @@ import 'package:async/async.dart';
 final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
-final String emailColumn = "phoneColumn";
+final String emailColumn = "emailColumn";
 final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
+class ContactHelper {
 
-class ContactHelper{ //usando o padrão de projeto singleton para evitar que o objeto seja instanciado mais de uma vez
-  static final ContactHelper  _instance = ContactHelper.internal();
+  static final ContactHelper _instance = ContactHelper.internal();
 
   factory ContactHelper() => _instance;
 
   ContactHelper.internal();
 
-  Database _db; //Apenas a classe contact helper vai poder mexer na classe
+  Database _db;
 
-  Future<Database>get db async{
-    if (_db != null){
+  Future<Database> get db async {
+    if(_db != null){
       return _db;
-
-    }else {
+    } else {
       _db = await initDb();
       return _db;
     }
   }
 
   Future<Database> initDb() async {
-    final databasePath = await getDatabasesPath();
-    final path = join(databasePath, "contactsnew.db");
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contactsnew.db");
 
-    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{//será realizado apenas na primeira vez para iniciar o banco
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
       await db.execute(
-        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
+              "$phoneColumn TEXT, $imgColumn TEXT)"
       );
-
     });
   }
-  
-  Future<Contact> saveContact(Contact contact) async{
+
+  Future<Contact> saveContact(Contact contact) async {
     Database dbContact = await db;
     contact.id = await dbContact.insert(contactTable, contact.toMap());
     return contact;
   }
-  
-  Future<Contact> getContact (int id) async{
-    Database dbContact = await db;
-    List<Map> maps= await dbContact.query(contactTable,
-    columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
-     where:"$idColumn = ?",
-    whereArgs: [id]);
 
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]);
     if(maps.length > 0){
       return Contact.fromMap(maps.first);
-
-    }else{
+    } else {
       return null;
     }
   }
 
-  Future<int>deleteContact (int id)async {
+  Future<int> deleteContact(int id) async {
     Database dbContact = await db;
-   await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
-
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
   }
 
-  Future<int>updateContact (Contact contact) async{
+  Future<int> updateContact(Contact contact) async {
     Database dbContact = await db;
-    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?", whereArgs: [contact.id]);
+    return await dbContact.update(contactTable,
+        contact.toMap(),
+        where: "$idColumn = ?",
+        whereArgs: [contact.id]);
   }
 
-  Future<List>getAllContacts() async{
+  Future<List> getAllContacts() async {
     Database dbContact = await db;
-    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");//lista de mapas
-    List<Contact> listContact = List();//declarada uma lista de contatos
-    for (Map m in listMap ){//pra cada mapa da minha lista de mapas
-      listContact.add(Contact.fromMap(m));//transformo em um contato
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    List<Contact> listContact = List();
+    for(Map m in listMap){
+      listContact.add(Contact.fromMap(m));
     }
     return listContact;
   }
 
-  Future<int>getNumber() async{
+  Future<int> getNumber() async {
     Database dbContact = await db;
     return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
   }
 
- Future close () async{
+  Future close() async {
     Database dbContact = await db;
     dbContact.close();
   }
@@ -103,7 +102,9 @@ class Contact {
   String phone;
   String img;
 
-  Contact.fromMap (Map map){//armazena os contatos no formato de mapa
+  Contact();
+
+  Contact.fromMap(Map map){
     id = map[idColumn];
     name = map[nameColumn];
     email = map[emailColumn];
@@ -111,23 +112,22 @@ class Contact {
     img = map[imgColumn];
   }
 
-//transforma o contato em mapa
-
-Map toMap(){
+  Map toMap() {
     Map<String, dynamic> map = {
       nameColumn: name,
       emailColumn: email,
       phoneColumn: phone,
       imgColumn: img
     };
-    if (id != null){ // o id é adicionado pelo banco de dados
+    if(id != null){
       map[idColumn] = id;
     }
     return map;
- }
+  }
 
-  @override //para dar print no contato
+  @override
   String toString() {
     return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lista_de_contatos/ui/contact_dart.dart';
 import 'package:lista_de_contatos/ui/home_dart.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
